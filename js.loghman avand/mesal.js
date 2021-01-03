39bb5cd18309b876eb568752abe600e94fafa9ee
mmhp14 <mmhp16@gmail.com>
2021-01-03 15:50:37 +0330

    edameye js..

--- js.loghman avand/mesal.js
+++ js.loghman avand/mesal.js
@@ -93,17 +93,130 @@ console.log(f1(...e));
 
 //    ---------------------------------------------------------------------------
 function f2() {
-    var n = document.getElementById("inFactor").value;
-    var m = document.getElementById("result").value;
+    var k = document.getElementById("inFactor").value;
+    document.getElementById("result").value = factorial(k);
+}
 
-    function factorial(n) {
+function factorial(n) {
 
-        if (n == 1) {
-            return 1;
+    if (n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+
+}
+
+//  ---------------------------------------------------------------------------
+function f3() {
+    var num = document.getElementById("inNum").value;
+    var expo = document.getElementById("inExpo").value;
+    document.getElementById("res").value = tavan(num, expo);
+}
+
+
+function tavan(n, m) {
+    if (m == 1) {
+        return n;
+    }
+    return n * tavan(n, m - 1)
+};
+
+//  -----------------------------------------------------------------------------------
+
+
+function Kia(m, n) {
+    this.model = m;
+    this.year = 2020;
+    this.color = n;
+    this.option = "full option";
+    return "my favorite car is " + this.model + this.year + " whit " + this.color + " and " + this.option;
+}
+
+var mashinMehdi = new Kia("Optima", "black");
+mashinMehdi.color = "blue";
+
+var mashinMatina = new Kia("Cadenza", "white");
+
+document.getElementById("p1").innerHTML = mashinMatina.color;
+document.getElementById("p2").innerHTML = mashinMehdi.year;
+console.log(mashinMatina.color);
+
+
+//-----------------------------------------------------------------------------------------------------
+
+//می خواهیم خودرویی طراحی کنیم که اگر سوخت ان gas باشد سرعت ماشین در 0/7 ضرب شود .
+function Car(m, n, engine, sookht) {
+    this.color = "black";
+    this.soratOmomi = 200;
+    this.year = m;
+    this.model = n;
+    this.enginVolume = engine;
+    this.sookht = sookht;
+    this.speed = function () {
+        var sorat;
+        if (this.sookht == "gas") {
+            sorat = this.soratOmomi * 0 / 7;
+        } else {
+            sorat = soratOmomi;
+            return sorat;
         }
-        console.log(n * factorial(n - 1));
-        return n * factorial(n - 1);
-        // document.getElementById("result").value =
+    }
+
+}
+
+console.log(Car(2020, cerato, en, gas));
+
+// در نظر داریم خودروی فوق بعد از 1600 به ازای هر 200 عدد افزایش قدرت موتور سرعتش در 0/1 ضرب شود.
+
+
+function Car(m, n, engine, sookht) {
+    this.color = "black";
+    this.soratOmomi = 200;
+    this.year = m;
+    this.model = n;
+    this.enginVolume = engine;
+    this.sookht = sookht;
+    this.speed = function () {
+        var sorat;
+        if (this.sookht == "gas") {
+            sorat = this.soratOmomi * 0 / 7;
+        } else {
+            sorat = soratOmomi;
+            return sorat;
+        }
+        var a = ((((this.enginVolume - 1600) / 200) * 0 / 1) * sorat) + sorat;
+        return a;
+    }
+
+}
+
+//در مثال بالا می خواهیم پارامتر دیگری را لحاظ کنیم که از سمت کاربر وارد میشود و روی سرعت خودرو تاثیر دارد.
+
+
+function Car(m, n, engine, sookht) {
+    this.color = "black";
+    this.soratOmomi = 200;
+    this.year = m;
+    this.model = n;
+    this.enginVolume = engine;
+    this.sookht = sookht;
+    this.speed = function (tireVolume) {
+        var sorat;
+        if (!tireVolume) {
+            tireVolume = 1;
+        }
+        if (this.sookht === "gas") {
+            sorat = this.soratOmomi * 0 / 7;
+        } else {
+            sorat = soratOmomi;
+            return sorat;
+        }
+        var a = ((((this.enginVolume - 1600) / 200) * 0 / 1) * sorat) + sorat;
+        tireVolume = (tireVolume > 1) ? 1 : tireVolume;
+        return a * tireVolume;
     }
 }
 
+var car1 = new Car(2020, "cerato", 1600, "gas");
+console.log(car1.speed(0 / 9));
+console.log(car1.color);
